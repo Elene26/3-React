5037febb2cdcfca02b274b65f7ab44824a47167f
Elene Ung <ung.elene@gmail.com>
2020-04-03 21:10:11 -0700

    Controlled Forms

---
+++ nucampsite/trash.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+const card = {
+    suit: "Diamonds",
+    number: 12
+};
+
+const cardSuit = car.suit
+
+console.log(cardSuit);
+
+const {suit, number} = card;
+
+console.log(suit)
+console.log(number)
