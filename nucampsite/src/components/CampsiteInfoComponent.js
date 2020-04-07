ba3ec21dbccf980320b8eb9f6075076cf6ca49bd
Elene Ung <ung.elene@gmail.com>
2020-04-07 11:28:33 -0700

    Week 4 Assignment

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,7 +1,91 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        }
+    }
+
+    toggleModal = () => {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit = (values) => {
+        this.toggleModal();
+        console.log('Current State is: ' + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <Button outline onClick={this.toggleModal}>
+                        <i className="fa fa-pencil fa-lg" /> Submit Comment
+                    </Button>
+
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                                <div className='form-group'>
+                                    <Label>
+                                        Rating
+                                    </Label>
+                                    <Control.select model='.rating' name="rating" id='rating' className='form-control'>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </div>
+                                <div className='form-group' >
+                                    <Control.text model='.author' name='author' id='author' placeholder='Name' className='form-control'
+                                        validators={{
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }} />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </div>
+
+                                <div className='form-group'>
+                                    <Control.textarea
+                                        model='text' name='text' id='text' rows='6' className='form-control'
+                                    />
+                                </div>
+                                <Button type="submit" color="primary">
+                                    Submit Comment
+                                </Button>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                </div>
+            </div>
+        )
+    }
+
+}
 
 function RenderCampsite({ campsite }) {
     {
@@ -19,24 +103,24 @@ function RenderCampsite({ campsite }) {
 }
 
 function RenderComments({ comments }) {
-    {
-        if (comments) {
+    if (comments) {
+        const finalComments = comments.map((comment) => {
             return (
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map(comment => {
-                        return (
-                            <div key={comment.id}>
-                                <p>{comment.text}<br />
-                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                            </div>
-                        );
-                    })}
+                <div key={comment.id}>
+                    <p>{comment.text}<br />
+                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                 </div>
             );
-        }
-        return <div />
+        });
+        return (
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {finalComments}
+                <CommentForm />
+            </div>
+        )
     }
+    return <div />;
 }
 
 function CampsiteInfo(props) {
@@ -64,3 +148,7 @@ function CampsiteInfo(props) {
 }
 
 export default CampsiteInfo;
+
+
+
+
